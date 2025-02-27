bef79f95b64136b91060ea20dfad96479eafea83
dpcnguyen <dpcnguyen5690@gmail.com>
2025-02-27 22:45:01 +0700

    ok

---
+++ 20times.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 25 23:00:32 2021
+
+@author: Nguyen Dinh
+"""
+
+
+import euler_subsonic_nguyen
+import euler_exact_nguyen
+import maxwell_orthotropic_nguyen
+import maxwell_orthotropic_exact_nguyen
+
+euler_subsonic_nguyen
+euler_exact_nguyen
+maxwell_orthotropic_nguyen
+maxwell_orthotropic_exact_nguyen
